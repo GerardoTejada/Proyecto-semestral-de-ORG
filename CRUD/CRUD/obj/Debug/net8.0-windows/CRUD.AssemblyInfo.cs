80991cf271566829fb4e7069cfc04232a3e40d45
Gerardo <gerardo.tejada@utp.ac.pa>
2025-12-16 23:47:26 -0500

    cambios en el diseño dle interfaz

--- CRUD/CRUD/obj/Debug/net8.0-windows/CRUD.AssemblyInfo.cs
+++ CRUD/CRUD/obj/Debug/net8.0-windows/CRUD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e36afacab9729b778984d7ed0c4a9909ed0a3543")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
